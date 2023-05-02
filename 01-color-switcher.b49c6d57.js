ebfd0993eeb993184ad2446cd58d1913f1e15f9b
Favixx <125205449+Favixx@users.noreply.github.com>
2023-05-02 12:58:14 +0000

    Deploying to gh-pages from @ Favixx/goit-js-hw-09@4f92fb14f20925641bf1f84b58e4b2389ac96e0b 🚀

---
+++ 01-color-switcher.b49c6d57.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]"),r=document.querySelector("body");function d(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));r.style.backgroundColor=t}e.addEventListener("click",(function(){d(),t=setInterval(d,1500),e.setAttribute("disabled","disabled")})),a.addEventListener("click",(function(){clearInterval(t),e.removeAttribute("disabled","disabled")}))}();
+//# sourceMappingURL=01-color-switcher.b49c6d57.js.map
