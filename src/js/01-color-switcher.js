4f92fb14f20925641bf1f84b58e4b2389ac96e0b
Favixx <bogdandom52@gmail.com>
2023-05-02 15:57:48 +0300

    task 1 is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
-
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+const startChanging = document.querySelector('[data-start]');
+const stopChanging = document.querySelector('[data-stop]');
+const body = document.querySelector('body')
+let timeoutId
+function colorChanger(){
+    const color = getRandomHexColor()
+    body.style.backgroundColor = color;
+}
+startChanging.addEventListener("click", ()=>{
+    colorChanger()
+    timeoutId = setInterval(colorChanger, 1500);
+    startChanging.setAttribute("disabled", "disabled")
+})
+stopChanging.addEventListener("click", ()=>{
+    clearInterval(timeoutId);
+    startChanging.removeAttribute("disabled", "disabled");
+})
